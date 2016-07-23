239951a24b07d1d8d1be0607f6b733dd1bed7412
Shannon Riester <shannonriester@gmail.com>
2016-07-23 15:04:23 -0500

    fixed issues with current session reloading

--- app/scripts/views/placeItemView.js
+++ app/scripts/views/placeItemView.js
@@ -1,16 +1,21 @@
 import $ from 'jquery';
 import Backbone from 'backbone';
 
+import session from '../models/sessionModel';
+
 const PlaceItemView = Backbone.View.extend({
   tagName : 'li',
   className : 'post-item-li',
   template : function() {
+    // console.log(this.model);
+    // console.log(session);
     return `
       <h3>
       <a href="#posts/${this.model.get('id')}">${this.model.get('title')}</a>
       </h3>
       <img src="${this.model.get('imgurl')}" class="place-item-image" />
-      <p class="username">${this.model.get('username')}</p>
+      <p class="username">${session.get('name')}</p>
+      <p class="username">${session.get('username')}</p>
     `;
   },
   render : function() {
