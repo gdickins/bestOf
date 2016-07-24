cfc0e7fcd58865bf2cffaa5e28daedab35c192d2
Shannon Riester <shannonriester@gmail.com>
2016-07-24 13:35:16 -0500

    working logout now

--- app/scripts/views/logoutView.js
+++ app/scripts/views/logoutView.js
@@ -11,15 +11,22 @@ events:{
   'click .logout-btn': 'logoutFunction'
 },
 logoutFunction: function(evt){
-  console.log(session);
+  // console.log(session);
+  // console.log(session.attributes.authtoken);
+  // console.log(session.attributes.email);
+  // console.log(session.attributes.name);
+  // console.log(session.attributes.userId);
   evt.preventDefault();
+
   session.save(null, {
     url: `https://limitless-falls-88798.herokuapp.com/login`,
     success: (model, response) => {
       model.unset('password');
+      // console.log(session);
       localStorage.removeItem('authtoken');
-      localStorage.removeItem('userId');
+      localStorage.removeItem('email');
       localStorage.removeItem('name');
+      localStorage.removeItem('userId');
       console.log('User logged out!');
       router.navigate('login', {trigger:true});
     }
