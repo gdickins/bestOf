463621b96f2f7444851c4e2a50beabcd4e11edfe
Shannon Riester <shannonriester@gmail.com>
2016-07-24 23:05:58 -0500

    final push for the night

--- app/scripts/views/placeView.js
+++ app/scripts/views/placeView.js
@@ -40,7 +40,7 @@ const PlaceView = Backbone.View.extend({
       <img src="${this.model.get('imgurl')}" class="place-item-image"/>
       <p>${this.model.get('address')}</p>
       <p class="username">Submitted by: ${userObj.get('name')}, AKA "${userObj.get('username')}"</p>
-      <button class="vote-btn">Vote </button><span>${this.model.get('vote_count')}</span>
+      <button class="vote-btn">Vote </button><span>${this.model.get('NewRank')}</span>
     `;
   },
   render : function() {
